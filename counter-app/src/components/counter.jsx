4b21b3755a4dd10ed31a890089eb7288af48db85
Aditya Singgih <adityasinggih@msn.com>
2018-11-27 11:16:41 +1100

    React fragment instead of div utilisation

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 	render() {
-		return <h1>Hello World</h1>;
+		/* 
+		We can use React.Fragment instead of div tag when returning
+		multiple html items. This prevents the creation of additional and
+		redundant div tags in the sourcecode.
+		*/
+		return (
+			<React.Fragment>		
+				<h1>Hello World</h1>
+				<button>Increment</button>
+			</React.Fragment>
+		);		
 	}
 }
 
 export default Counter
+
