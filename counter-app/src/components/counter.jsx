7497de16c6de739df9c45d89e27398d5cc38bcb6
Aditya Singgih <adityasinggih@msn.com>
2018-11-27 13:39:07 +1100

    Setting attributes e.g., style or bootstrap classes

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -12,10 +12,14 @@ class Counter extends Component {
 			redundant div tags in the sourcecode.
 		*/
 		return (
-			<React.Fragment>		
-				<span>{ this.formatCount() }</span>
-				<button>Increment</button>
-			</React.Fragment>
+			<div>
+				{/* 
+					We have to use className for span / div class name because
+					class is obviously a reserved keyword in jsx.
+				*/}
+				<span className="badge badge-primary m-2">{ this.formatCount() }</span> 
+				<button className="btn btn-secondary btn-sm">Increment</button>
+			</div>
 		);		
 	}
 
