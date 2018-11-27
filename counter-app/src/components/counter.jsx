a7c85e9ca4194b10354f2cce2ce9fa7b02527ef1
Aditya Singgih <adityasinggih@msn.com>
2018-11-27 11:55:27 +1100

    Embedding expressions

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+	state = {
+		count: 0,
+	};
+
 	render() {
 		/* 
-		We can use React.Fragment instead of div tag when returning
-		multiple html items. This prevents the creation of additional and
-		redundant div tags in the sourcecode.
+			We can use React.Fragment instead of div tag when returning
+			multiple html items. This prevents the creation of additional and
+			redundant div tags in the sourcecode.
 		*/
 		return (
 			<React.Fragment>		
-				<h1>Hello World</h1>
+				<span>{ this.formatCount() }</span>
 				<button>Increment</button>
 			</React.Fragment>
 		);		
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		return count == 0 ? 'Zero' : count;
 	}
 }
 
