9175f96481f6ff33fe9371593ec62a5666de5466
Aditya Singgih <adityasinggih@msn.com>
2018-11-28 23:33:34 +1100

    Passing data to components

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 	state = {
-		count: 0,
-		tags: ['t1', 't2', 't3']
+		value: this.props.value,
 	};
 
 	// constructor(){
@@ -34,21 +33,20 @@ class Counter extends Component {
 			We're passing a new state object (or override, if it already exist),
 			to the current Class' state.
 		*/
-		console.log(product);
-		this.setState( { count: this.state.count + 1 } );
+		this.setState( { value: this.state.value + 1 } );
 	}
 
 	getBadgeClasses() {
 		// let is used instead of var, because we only need 
 		// it within this scope, not global
 		let classes = "badge m-2 badge-";
-		classes += this.state.count === 0 ? "warning" : "primary";
+		classes += this.state.value === 0 ? "warning" : "primary";
 		return classes;
 	}
 
 	formatCount() {
-		const { count } = this.state;
-		return count === 0 ? 'Zero' : count;
+		const { value } = this.state;
+		return value === 0 ? 'Zero' : value;
 	}
 
 	renderList() {
