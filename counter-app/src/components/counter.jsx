a3b788593500d0ba310fb2577a5e118152f1d2eb
Aditya Singgih <adityasinggih@msn.com>
2018-11-30 12:40:24 +1100

    Raising and Handling Events

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
 	state = {
-		value: this.props.value,
+		value: this.props.value
 	};
 
 	// constructor(){
@@ -20,26 +20,39 @@ class Counter extends Component {
 					We have to use className for span / div class name because
 					class is obviously a reserved keyword in jsx.
 				*/}
-				<span className={ this.getBadgeClasses() }>{ this.formatCount() }</span> 
-				<button onClick = { () => this.handleIncrement({id: 1}) } className="btn btn-secondary btn-sm">Increment</button>
+				<span className={this.getBadgeClasses()}>
+					{this.formatCount()}
+				</span>
+				<button
+					onClick={this.handleIncrement}
+					className="btn btn-secondary btn-sm"
+				>
+					Increment
+				</button>
+				<button
+					onClick={this.props.onDelete}
+					className="btn btn-danger btn-sm m-2"
+				>
+					Delete
+				</button>
 			</div>
-		);		
+		);
 	}
 
 	// doHandleIncrement () {
 	// 	this.handleIncrement({ id:1 });
 	// }
 
-	handleIncrement = (product) => {
+	handleIncrement = () => {
 		/* 
 			We're passing a new state object (or override, if it already exist),
 			to the current Class' state.
 		*/
-		this.setState( { value: this.state.value + 1 } );
-	}
+		this.setState({ value: this.state.value + 1 });
+	};
 
 	getBadgeClasses() {
-		// let is used instead of var, because we only need 
+		// let is used instead of var, because we only need
 		// it within this scope, not global
 		let classes = "badge m-2 badge-";
 		classes += this.state.value === 0 ? "warning" : "primary";
@@ -48,25 +61,8 @@ class Counter extends Component {
 
 	formatCount() {
 		const { value } = this.state;
-		return value === 0 ? 'Zero' : value;
-	}
-
-	renderList() {
-		if (this.state.tags.length === 0) {
-			return <p>No tags here!</p>;
-		}
-		return <ul>{ this.mapToList() }</ul>;
-	}
-
-	mapToList() {
-		/* 
-		This code below basically puts each item inside
-		the tags array into <li> html objects
-		*/
-		const tags = this.state.tags;
-		return tags.map(tag => <li key={tags.indexOf(tag)}>{tag}</li>);
+		return value === 0 ? "Zero" : value;
 	}
 }
 
 export default Counter;
-
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import Counter from './counter';
+import React, { Component } from "react";
+import Counter from "./counter";
 
 class Counters extends Component {
-    state = {
-        counters: [
-            { id: 1, value: 0 },  // unique identifier for each counter, and initial value
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 }
-        ]
-    }
-    render() {
-        return (
-            <div>
-                { this.state.counters.map( counter => 
-                    <Counter key={counter.id} value={counter.value} id={counter.id}>
-                        {/* <h3>Counter #{counter.id}</h3> */}
-                    </Counter>  
-                )}
-            </div>
-        );
-    }
+	state = {
+		counters: [
+			{ id: 1, value: 0 }, // unique identifier for each counter, and initial value
+			{ id: 2, value: 0 },
+			{ id: 3, value: 0 },
+			{ id: 4, value: 0 }
+		]
+	};
+
+	// we want to pass a reference to this method
+	// using props to our child component.
+	handleDelete = () => {
+		console.log("event handler called");
+	};
+
+	render() {
+		return (
+			<div>
+				{this.state.counters.map(counter => (
+					<Counter
+						key={counter.id}
+						onDelete={this.handleDelete}
+						value={counter.value}
+						id={counter.id}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default Counters;
