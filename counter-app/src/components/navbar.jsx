ca42aee164fcf5d06c44ffc978045dec2db6e067
Aditya Singgih <adityasinggih@msn.com>
2018-12-04 16:31:51 +1100

    Navbar total display made cleaner

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
-// Stateless Functional Component
-const Navbar = ({ totalCounters }) => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<a className="navbar-brand" href=".">
-				Navbar{" "}
-				<span className="badge badge-pill badge-secondary">
-					{totalCounters}
-				</span>
-			</a>
-		</nav>
-	);
-};
+class Navbar extends Component {
+	// Display nothing if totalCounters is 0
+	displayTotal = () => {
+		/* this is equal to
+		
+		if (this.props.totalCounters === 0) {
+			return "";
+		}
+		return this.props.totalCounters;
+		
+		*/
+
+		return this.props.totalCounters === 0 ? "" : this.props.totalCounters;
+	};
+
+	render() {
+		return (
+			<div>
+				<nav className="navbar navbar-light bg-light">
+					<a className="navbar-brand" href=".">
+						Navbar{" "}
+						<span
+							className={
+								this.props.totalCounters === 0
+									? ""
+									: "badge badge-pill badge-secondary" // Only display css when counter is not 0
+							}
+						>
+							{this.displayTotal()}
+						</span>
+					</a>
+				</nav>
+			</div>
+		);
+	}
+}
 
 export default Navbar;
