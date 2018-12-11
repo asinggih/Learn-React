8e804d35084ab3c70ce078730ae07dd1e33f2542
Aditya Singgih <adityasinggih@msn.com>
2018-12-11 14:41:58 +1100

    api call using Axios

--- pics-app/src/App.js
+++ pics-app/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
-import "./App.css";
-import "./component/searchbar";
+import axios from "axios";
 import SearchBar from "./component/searchbar";
 
 class App extends Component {
 	handleSearchSubmit = searchTerm => {
-		console.log(searchTerm);
+		/* 
+		
+		axios has 2 arguments: URL, and objects that can be used to customies the request
+		
+		The request below will produce something like
+		https://api.unsplash.com/search/photos?query={OUR SEARCH TERM}
+
+		*/
+		axios.get("https://api.unsplash.com/search/photos", {
+			params: { query: searchTerm },
+			headers: {
+				Authorization:
+					"Client-ID 2cf69ccc2d5dd3c76b56227ad5e47f480c8aca2564adb0ab23f000abd65b278e"
+			}
+		});
 	};
 
 	render() {
