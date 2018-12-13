bf0768a74ab53f53fa86ae02bafc08f37bab7115
Aditya Singgih <adityasinggih@msn.com>
2018-12-13 17:26:45 +1100

    Added imagelist.css to imagelist comopnent style

--- pics-app/src/component/imagelist.jsx
+++ pics-app/src/component/imagelist.jsx
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
+import "./imagelist.css";
+import ImageCard from "./imagecard";
 
 class ImageList extends Component {
 	mapImageUrl() {
 		const images = this.props.images;
-		/*
-        Below is a destructured version of 
-        ... image => (
-            ... key={image.id}
-        etc,
-        )
-        */
-		return images.map(({ id, urls, description }) => (
-			<img key={id} src={urls.regular} alt={description} />
-		));
+		return images.map(image => <ImageCard key={image.id} image={image} />);
 	}
 
 	render() {
-		return this.mapImageUrl();
+		return <div className="image-list">{this.mapImageUrl()}</div>;
 	}
 }
 
