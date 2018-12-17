0996998fc178f797925107e755920425ba59ec27
Aditya Singgih <adityasinggih@msn.com>
2018-12-17 17:20:49 +1100

    moved App.js into components folder

---
+++ songs/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+/*
+  Since we're gonna use redux, we will have less class-based components,
+  since we will store most of the states on redux instead of the classes itself.
+*/
+const App = () => {
+	return <div>App</div>;
+};
+
+export default App;
