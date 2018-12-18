7ed3ee3b8cc8b11c73a39a356e40022cd525ff29
Aditya Singgih <adityasinggih@msn.com>
2018-12-18 13:48:24 +1100

    created redux store and passed it into Provider as props

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 /*---------------------------------------------
                 Reducers
 ----------------------------------------------*/
@@ -30,3 +32,9 @@ const selectedSongReducer = (selectedSong = null, action) => {
 	}
 	return selectedSong;
 };
+
+export default combineReducers({
+	// Need to import this from redux first
+	songs: songsReducer,
+	selectedSong: selectedSongReducer
+});
