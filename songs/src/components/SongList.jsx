8b664de95926fe25c36d83b8ee10095a63bb1de9
Aditya Singgih <adityasinggih@msn.com>
2018-12-18 17:10:20 +1100

    added select song action creator into the connect function

--- songs/src/components/SongList.jsx
+++ songs/src/components/SongList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
 	renderSongList() {
@@ -13,7 +14,12 @@ class SongList extends Component {
 			return (
 				<div className="item" key={songs.indexOf(song)}>
 					<div className="right floated content">
-						<button className="ui button primary">Select</button>
+						<button
+							className="ui button primary"
+							onClick={() => this.props.selectSong(song)}
+						>
+							Select
+						</button>
 					</div>
 
 					<div className="content">{song.title}</div>
@@ -30,7 +36,11 @@ class SongList extends Component {
 // we can name this function anything, we
 // use mapStateToProps just because of the naming convention
 const mapStateToProps = state => {
+	console.log(state);
 	return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+	mapStateToProps,
+	{ selectSong: selectSong }
+)(SongList);
