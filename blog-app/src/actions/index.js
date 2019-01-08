f18300cd61758c5790f0b483b88e689a54030424
Aditya Singgih <adityasinggih@msn.com>
2019-01-08 16:24:02 +1100

    refactored fetchPosts function

--- blog-app/src/actions/index.js
+++ blog-app/src/actions/index.js
@@ -1,12 +1,11 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-	return async function(dispatch, getState) {
-		const response = await jsonPlaceholder.get("/posts");
+// getState is removed from the function argument because we don't acutally call it inside our function.
+export const fetchPosts = () => async dispatch => {
+	const response = await jsonPlaceholder.get("/posts");
 
-		dispatch({
-			type: "FETCH_POST",
-			payload: response
-		});
-	};
+	dispatch({
+		type: "FETCH_POST",
+		payload: response
+	});
 };
