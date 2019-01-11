6c7a3a5b4ed46e9fe02bf70e286ccbb099947342
Aditya Singgih <adityasinggih@msn.com>
2019-01-11 18:14:23 +1100

    created postReducer.js and added it into the combineReducers function

--- blog-app/src/reducers/index.js
+++ blog-app/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-	placeHolder: () => "placeHolder" // just to remove the redux error in console
+	posts: postsReducer
 });
