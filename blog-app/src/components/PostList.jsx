b1717e97227b205388b2cb86ab1aec66ffee3cab
Aditya Singgih <adityasinggih@msn.com>
2019-01-15 14:13:09 +1100

    added helper method to display the posts and general cleanup

--- blog-app/src/components/PostList.jsx
+++ blog-app/src/components/PostList.jsx
@@ -7,9 +7,33 @@ class PostList extends Component {
 		this.props.fetchPosts();
 	}
 
+	/**
+	 * This is just a helper method for rendering each
+	 * of the item inside this.props.posts to hv proper css
+	 * classes. It's just looping each of them
+	 *
+	 * All of the classes are taken from the Semantic UI Library
+	 */
+	renderList() {
+		return this.props.posts.map(post => {
+			return (
+				<div className="item" key={post.id}>
+					<i className="large middle aligned icon user" />
+					<div className="content">
+						<div className="description">
+							<h2>{post.title}</h2>
+							<p>{post.body}</p>
+						</div>
+					</div>
+				</div>
+			);
+		});
+	}
+
 	render() {
-		console.log(this.props.posts);
-		return <div>PostList</div>;
+		return (
+			<div className="ui relaxed divided list">{this.renderList()}</div>
+		);
 	}
 }
 
