f2103d1211ff16a7b912e80094437e387b308dc2
Aditya Singgih <adityasinggih@msn.com>
2019-01-15 14:57:16 +1100

    added fetchUser action creator in actions

--- blog-app/src/actions/index.js
+++ blog-app/src/actions/index.js
@@ -2,10 +2,21 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 // getState is removed from the function argument because we don't acutally call it inside our function.
 export const fetchPosts = () => async dispatch => {
-	const response = await jsonPlaceholder.get("/posts");
+	const res = await jsonPlaceholder.get("/posts");
 
 	dispatch({
 		type: "FETCH_POST",
-		payload: response.data
+		payload: res.data
 	});
+};
+
+export const fetchUser = id => async dispatch => {
+	// The response of this api call will be an object, 
+	// instead of a list of objects like in fetchPosts
+	const res = await jsonPlaceholder.get(`/users/${id}`));
+
+	dispatch({
+		type: "FETCH_USER",
+		payload: res.data
+	})
 };
