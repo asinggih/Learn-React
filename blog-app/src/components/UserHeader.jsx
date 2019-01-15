d6f328dcfed71c2f6e21011cccaafceba8f84e66
Aditya Singgih <adityasinggih@msn.com>
2019-01-15 18:28:21 +1100

    Populated UserHeader with mapStateToProps and displaying the user

--- blog-app/src/components/UserHeader.jsx
+++ blog-app/src/components/UserHeader.jsx
@@ -8,11 +8,25 @@ class UserHeader extends Component {
 	}
 
 	render() {
-		return <div>User Header</div>;
+		const { users, userId } = this.props;
+
+		// find the user with the same user id as the one from
+		// this.props.userId, from the returned users array of the api call
+		const currentUser = users.find(user => user.id === userId);
+
+		return currentUser ? (
+			<div className="header">{currentUser.name}</div>
+		) : (
+			<div>Loading...</div>
+		);
 	}
 }
 
+const mapStateToProps = state => {
+	return { users: state.users };
+};
+
 export default connect(
-	null,
+	mapStateToProps,
 	{ fetchUser }
 )(UserHeader);
