679a9399e7a5ac18fc5c9ef55c4b65a22b1d2a42
Aditya Singgih <adityasinggih@msn.com>
2019-01-16 18:03:18 +1100

    used memoisation for fetchUser to avoid overfetching

--- blog-app/src/actions/index.js
+++ blog-app/src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
 
 // getState is removed from the function argument because we don't acutally call it inside our function.
 export const fetchPosts = () => async dispatch => {
@@ -10,7 +11,12 @@ export const fetchPosts = () => async dispatch => {
 	});
 };
 
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => {
+	_fetchUser(id, dispatch);
+};
+
+// Helper function for fetchUser to memoise the async call
+const _fetchUser = _.memoize(async (id, dispatch) => {
 	// The response of this api call will be an object,
 	// instead of a list of objects like in fetchPosts
 	const res = await jsonPlaceholder.get(`/users/${id}`);
@@ -19,4 +25,4 @@ export const fetchUser = id => async dispatch => {
 		type: "FETCH_USER",
 		payload: res.data
 	});
-};
+});
