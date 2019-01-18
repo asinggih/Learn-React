20a90b762131f7f5afa61dc63d694c3f9f564ba7
Aditya Singgih <adityasinggih@msn.com>
2019-01-18 15:39:36 +1100

    Example of using React Router

--- streams-app/client/src/components/App.js
+++ streams-app/client/src/components/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+	return <div>Page One Here</div>;
+};
+
+const PageTwo = () => {
+	return (
+		<div>
+			Page Two Here
+			<button>Click Here</button>
+		</div>
+	);
+};
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path="/" exact component={PageOne} />
+					<Route path="/pagetwo" component={PageTwo} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
