9f1ed1fb4ba9c02b850972c8979b8be4b8fc10d7
Aditya Singgih <adityasinggih@msn.com>
2019-01-30 19:37:57 +1100

    installed and added redux-form into reducer

--- streams-app/client/src/reducers/index.js
+++ streams-app/client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form"; // to avoid naming confusion
 import authReducer from "./authReducer";
 
+// we can access these items below inside mapStateToProps via state.KEYNAME
 export default combineReducers({
-	authentication: authReducer
+  authentication: authReducer,
+  form: formReducer // redux form reducer key MUST BE NAMED as "form"
 });
