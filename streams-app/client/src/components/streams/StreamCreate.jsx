e03a899192503dca2aeac75259b9dcd0218ac787
Aditya Singgih <adityasinggih@msn.com>
2019-02-06 14:13:57 +0700

    made input fields prettier and handled the form submission

--- streams-app/client/src/components/streams/StreamCreate.jsx
+++ streams-app/client/src/components/streams/StreamCreate.jsx
@@ -2,17 +2,39 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-	renderInput({ input }) {
+	renderInput({ input, inputLabel }) {
 		// Destructuring from formProps
-		return <input {...input} />;
+		return (
+			<div className="field">
+				<label>{inputLabel}</label>
+				<input {...input} />
+			</div>
+		);
+	}
+
+	onSubmit(formValues) {
+		// We can do whatever with our form values in here. input sanitation etc.
+		console.log(formValues);
 	}
 
 	render() {
-		// console.log(this.props);
 		return (
-			<form>
-				<Field name="title" component={this.renderInput} />
-				<Field name="description" component={this.renderInput} />
+			<form
+				// Submission is handled using redux-form handleSubmit prop
+				onSubmit={this.props.handleSubmit(this.onSubmit)}
+				className="ui form"
+			>
+				<Field
+					name="title"
+					component={this.renderInput}
+					inputLabel="Enter Title"
+				/>
+				<Field
+					name="description"
+					component={this.renderInput}
+					inputLabel="Enter Description"
+				/>
+				<button className="ui button primary">Submit</button>
 			</form>
 		);
 	}
