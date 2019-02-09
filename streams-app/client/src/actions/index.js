006f0877c15e6d672a46a34a5eb464835bd9e056
Aditya Singgih <adityasinggih@msn.com>
2019-02-09 17:47:02 +0700

    Finished writing the action creators

--- streams-app/client/src/actions/index.js
+++ streams-app/client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+	SIGN_IN,
+	SIGN_OUT,
+	CREATE_STREAM,
+	FETCH_STREAMS,
+	FETCH_STREAM,
+	EDIT_STREAM,
+	DELETE_STREAM
+} from "./types";
 
 export const signIn = userId => {
 	return {
@@ -25,5 +33,41 @@ export const createStream = formValues => async dispatch => {
 	dispatch({
 		type: CREATE_STREAM,
 		payload: res.data
+	});
+};
+
+export const fetchStreams = () => async dispatch => {
+	const res = await streams.get("/streams");
+	dispatch({
+		type: FETCH_STREAMS,
+		payload: res.data
+	});
+};
+
+export const fetchStream = id => async dispatch => {
+	const res = await streams.get(`/streams/${id}`); // backticks let us interpolate
+
+	dispatch({
+		type: FETCH_STREAM,
+		payload: res.data
+	});
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+	const res = await streams.put(`/streams/${id}`, formValues);
+	dispatch({
+		type: EDIT_STREAM,
+		payload: res.data
+	});
+};
+
+export const deleteStream = id => async dispatch => {
+	// no response cos we dont need it
+
+	await streams.delete(`/streams/${id}`);
+
+	dispatch({
+		type: DELETE_STREAM,
+		payload: id
 	});
 };
