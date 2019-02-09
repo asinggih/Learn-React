2f7a28d707a6fe12f4edd2c954eac24edbd42133
Aditya Singgih <adityasinggih@msn.com>
2019-02-09 17:23:56 +0700

    properly written createStream action creator to work with redux-thunk

--- streams-app/client/src/actions/index.js
+++ streams-app/client/src/actions/index.js
@@ -1,4 +1,5 @@
-import { SIGN_IN, SIGN_OUT } from "./types";
+import streams from "../apis/streams";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
 
 export const signIn = userId => {
 	return {
@@ -11,4 +12,18 @@ export const signOut = () => {
 	return {
 		type: SIGN_OUT
 	};
+};
+
+export const createStream = formValues => async dispatch => {
+	const res = await streams.post("/streams", formValues);
+
+	/**
+	 * Dispatching manually because we're utilising redux-thunk,
+	 * due to the use of async await that returns a request object,
+	 * instead of a plain object
+	 */
+	dispatch({
+		type: CREATE_STREAM,
+		payload: res.data
+	});
 };
