8651d97bf8171d09b356b64f15aa93e92c38bd79
Aditya Singgih <adityasinggih@msn.com>
2019-02-22 12:11:08 +0700

    Managed to load stream details into StreamEdit component

--- streams-app/client/src/components/streams/StreamEdit.jsx
+++ streams-app/client/src/components/streams/StreamEdit.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = props => {
-	console.log(props);
-	return <div>StreamEdit</div>;
+class StreamEdit extends Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+
+	render() {
+		if (!this.props.stream) {
+			return <div>Loading..</div>;
+		}
+
+		return (
+			<div>
+				<h1>{this.props.stream.title}</h1>
+				<h2>{this.props.stream.description}</h2>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		stream: state.streams[ownProps.match.params.id]
+	};
 };
 
-export default StreamEdit;
+export default connect(
+	mapStateToProps,
+	{ fetchStream }
+)(StreamEdit);
