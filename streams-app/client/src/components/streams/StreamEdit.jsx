3200078b9d09f53ba8b5d0a84dd39cc4bab0ec26
Aditya Singgih <adityasinggih@msn.com>
2019-02-25 12:45:14 +0700

    refactored streamEdit so it works with StreamForm

--- streams-app/client/src/components/streams/StreamEdit.jsx
+++ streams-app/client/src/components/streams/StreamEdit.jsx
@@ -1,21 +1,46 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import _ from "lodash";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends Component {
 	componentDidMount() {
 		this.props.fetchStream(this.props.match.params.id);
 	}
 
+	onSubmit = formValues => {
+		this.props.editStream(this.props.match.params.id, formValues);
+	};
+
 	render() {
 		if (!this.props.stream) {
 			return <div>Loading..</div>;
 		}
-
+		console.log(this.props.stream);
 		return (
 			<div>
-				<h1>{this.props.stream.title}</h1>
-				<h2>{this.props.stream.description}</h2>
+				<h3>Edit Stream</h3>
+				<StreamForm
+					/**
+					 * we use title and description as keys, because
+					 * that's what we use in our Field name
+					 * */
+					// initialValues={{
+					// 	title: "EDIT ME",
+					// 	description: "EDIT ME TOO"
+					// }}
+					/**
+					 * lodash pick creates a new object, based on
+					 * the given keyname of a particular object
+					 * */
+					initialValues={_.pick(
+						this.props.stream,
+						"title",
+						"description"
+					)}
+					onSubmit={this.onSubmit}
+				/>
 			</div>
 		);
 	}
@@ -29,5 +54,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
 	mapStateToProps,
-	{ fetchStream }
+	{ fetchStream, editStream }
 )(StreamEdit);
